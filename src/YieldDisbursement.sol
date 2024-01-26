299057473c7dd953f06c415a7f45d0915219ddd9
Ron Turetzky <turetzkyron@gmail.com>
2024-01-26 17:43:54 +0100

    compiling version including votes upgradeable

--- src/YieldDisbursement.sol
+++ src/YieldDisbursement.sol
@@ -1,10 +1,23 @@
 // contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC20VotesUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+abstract contract Bread is ERC20VotesUpgradeable {
+    function claimYieldForDisbursement() virtual public;
+}
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNFT") {
+
+contract YieldDisburser is OwnableUpgradeable {
+    Bread public bread;
+    function initialize(address breadAddress) public initializer {
+        bread = Bread(breadAddress);
+        __Ownable_init(msg.sender);
     }
+
+    function claimYield() internal  {
+        bread.claimYieldForDisbursement();
+    }
+
 }
