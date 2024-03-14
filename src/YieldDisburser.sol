72f6e9c6022eb01455246d676b4c1a4b24235061
Ron Turetzky <turetzkyron@gmail.com>
2024-03-14 20:09:38 +0200

    reintroducing dropped code

--- src/YieldDisburser.sol
+++ src/YieldDisburser.sol
@@ -79,4 +79,48 @@ contract YieldDisburser is OwnableUpgradeable {
     function claimYield() internal {
         breadToken.claimYield(breadToken.yieldAccrued(), address(this));
     }
+    // #### Voted distribution functions 
+    function castVote(uint[] memory projectindex, uint[] memory percentages) internal {
+        require(projectindex.length == percentages.length);
+        require(projectindex.length == breadchainProjects.length);
+        // calculate the sum of the percentages
+        uint sum = 0;
+        for (uint i = 0; i < percentages.length; i++) {
+            sum += percentages[i];
+        }
+        // sum of percentages must be 100
+        require(sum == 1);
+
+        for (uint i = 0; i < projectindex.length; i++) {
+            breadchainProjectsYield[projectindex[i]] = percentages[i];
+        }
+
+    }
+
+   function computeScaledAverage(uint[][] memory vectors) public  view returns (uint[] memory) {
+        uint VECTOR_LENGTH = breadchainProjects.length;
+        uint[] memory sum;
+        uint[] memory average;
+        // Step 1: Calculate sum and then the average of each element
+        for(uint i = 0; i < VECTOR_LENGTH; i++) {
+            // Summing elements of the two vectors
+            sum[i] = vectors[0][i] + vectors[1][i];
+            // Calculating the average, scaled to maintain precision
+            average[i] = (sum[i] * SCALE) / VECTOR_LENGTH;
+        }
+
+        // Step 2: Calculate the total sum of the average vector
+        uint totalSum = 0;
+        for(uint i = 0; i < VECTOR_LENGTH; i++) {
+            totalSum += average[i];
+        }
+
+        // Step 3: Normalize the average vector so it sums up to 100 * SCALE
+        uint[] memory normalizedAverage;
+        for(uint i = 0; i < VECTOR_LENGTH; i++) {
+            normalizedAverage[i] = (average[i] * SCALE) / totalSum;
+        }
+
+        return normalizedAverage;
+    }
 }
