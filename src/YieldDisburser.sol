1f13c1eeef9ed5cf7f66925f80e3c13d5b3915cd
Ron Turetzky <turetzkyron@gmail.com>
2024-03-16 13:59:00 +0000

    refactoring and organizing code

--- src/YieldDisburser.sol
+++ src/YieldDisburser.sol
@@ -13,49 +13,88 @@ abstract contract Bread is ERC20VotesUpgradeable {
 contract YieldDisburser is OwnableUpgradeable {
     address[] public breadchainProjects;
     uint[] public breadchainProjectsYield;
-    uint constant SCALE = 1e6; // Scale factor to maintain precision
     Bread public breadToken;
     uint48 public lastClaimed;
     uint48 public duration;
     error AlreadyClaimed();
+    event BaseYieldDistributed(uint256 amount,address project);
     function initialize(address breadAddress) public initializer {
         breadToken = Bread(breadAddress);
         __Ownable_init(msg.sender);
     }
+    /// ##########################################
+    /// ## Public Functions ##
+    /// ##########################################
+    function distributeYield() public {
+        (bool _resolved /*bytes memory _data */, ) = resolveYieldDistribution();
+        claimYield();
+        uint256 balance = breadToken.balanceOf(address(this)) / 2; 
+        uint256 projectCount = breadchainProjects.length;
+        if (_resolved) {
+            _distributeBaseYield(balance, projectCount);
+            _distributedVotedYield(projectCount);
+        }
+        lastClaimed = Time.timestamp();
+    }
 
+     function castVote(
+        uint[] memory projectindex,
+        uint[] memory percentages
+    ) internal {
+        require(projectindex.length == percentages.length);
+        require(projectindex.length == breadchainProjects.length);
+        // calculate the sum of the percentages
+        uint sum = 0;
+        for (uint i = 0; i < percentages.length; i++) {
+            sum += percentages[i];
+        }
+        // sum of percentages must be 100
+        require(sum == 1);
+
+        for (uint i = 0; i < projectindex.length; i++) {
+            breadchainProjectsYield[projectindex[i]] = percentages[i];
+        }
+    }
+
+    /// ##########################################
+    /// ## View Functions ##
+    /// ##########################################
     function resolveYieldDistribution()
         public
         view
         returns (bool, bytes memory)
     {
         uint48 _now = Time.timestamp();
-        if (_now < lastClaimed + duration) revert AlreadyClaimed();
-        bytes memory ret = abi.encodePacked(this.distributeYield.selector);
-        return (true, ret);
-    }
-
-    function distributeYield() public {
-        (bool _resolved /*bytes memory _data */, ) = resolveYieldDistribution();
-        if (_resolved) {
-            _distributeYield();
-        }
-    }
-    function _distributeYield() internal {
-        claimYield();
-        lastClaimed = Time.timestamp();
-        uint256 balance = breadToken.balanceOf(address(this));
-        uint256 projectCount = breadchainProjects.length;
+        uint256 balance = (breadToken.balanceOf(address(this)) + breadToken.yieldAccrued()) / 2; 
         require(
             balance > breadchainProjects.length,
             "Yield too low to distribute"
         );
+        if (_now < lastClaimed + duration) revert AlreadyClaimed();
+        bytes memory ret = abi.encodePacked(this.distributeYield.selector);
+        return (true, ret);
+    }
+    /// ##########################################
+    /// ## Internal Functions ##
+    /// ##########################################
+
+    function _distributeBaseYield(uint256 balance,uint256 projectCount) internal {
+        uint256 baseYield = balance / projectCount;
         for (uint i = 0; i < projectCount; i++) {
-            // breadToken.transfer(breadchainProjects[i], balance / projectCount);
-            breadToken.transfer(breadchainProjects[i], 1);
+            breadToken.transfer(breadchainProjects[i], baseYield);
+            emit BaseYieldDistributed(baseYield, breadchainProjects[i]);
         }
     }
-
-    //## Only Owner Functions ##
+    function _distributedVotedYield(uint256 projectCount) internal {
+        for (uint i = 0; i < projectCount; i++) {
+        }
+    }
+    function claimYield() internal {
+        breadToken.claimYield(breadToken.yieldAccrued(), address(this));
+    }
+    /// ##########################################
+    /// ## Only Owner Functions ##
+    /// ##########################################
     function setDuration(uint48 _duration) public onlyOwner {
         require(_duration > 0, "Duration must be greater than 0");
         duration = _duration * 1 minutes;
@@ -74,27 +113,5 @@ contract YieldDisburser is OwnableUpgradeable {
             }
         }
     }
-    // ### BREAD Token Functions ###
-    function claimYield() internal {
-        breadToken.claimYield(breadToken.yieldAccrued(), address(this));
-    }
-    // #### Voted distribution functions
-    function castVote(
-        uint[] memory projectindex,
-        uint[] memory percentages
-    ) internal {
-        require(projectindex.length == percentages.length);
-        require(projectindex.length == breadchainProjects.length);
-        // calculate the sum of the percentages
-        uint sum = 0;
-        for (uint i = 0; i < percentages.length; i++) {
-            sum += percentages[i];
-        }
-        // sum of percentages must be 100
-        require(sum == 1);
-
-        for (uint i = 0; i < projectindex.length; i++) {
-            breadchainProjectsYield[projectindex[i]] = percentages[i];
-        }
-    }
+   
 }
