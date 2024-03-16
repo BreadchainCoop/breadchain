94358262be60ae3a9094944f490e9c1922135a7a
Ron Turetzky <turetzkyron@gmail.com>
2024-03-16 12:57:45 +0000

    updating deploy script to not use a proxy admin because it is initalized in the implementation of the transpart proxy

--- script/DeployYieldDisburser.s.sol
+++ script/DeployYieldDisburser.s.sol
@@ -11,13 +11,10 @@ import {YieldDisburser} from "../src/YieldDisburser.sol";
 contract DeployYieldDisburser is Script {
     function run() external {
         vm.startBroadcast();
-        ProxyAdmin proxyAdmin = new ProxyAdmin(
-            address(0x86213f1cf0a501857B70Df35c1cb3C2EcF112844)
-        );
         YieldDisburser yieldDisburserImplementation = new YieldDisburser();
         TransparentUpgradeableProxy yieldDisburser = new TransparentUpgradeableProxy(
                 address(yieldDisburserImplementation),
-                address(proxyAdmin),
+                address(msg.sender),
                 abi.encodeWithSelector(
                     YieldDisburser.initialize.selector,
                     0xa555d5344f6FB6c65da19e403Cb4c1eC4a1a5Ee3
