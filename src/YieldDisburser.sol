66e840f4dca98e7e1d283d6a8bdde909c9cfdad5
Ron Turetzky <turetzkyron@gmail.com>
2024-05-18 17:40:20 +0300

    fix: adding revert for setting 0 block time

--- src/YieldDisburser.sol
+++ src/YieldDisburser.sol
@@ -400,6 +400,7 @@ contract YieldDisburser is OwnableUpgradeable {
     }
 
     function setBlockTime(uint256 _blockTime) public onlyOwner {
+        require(_blockTime > 0, "Block time must be greater than 0");
         blockTime = _blockTime;
     }
 }
