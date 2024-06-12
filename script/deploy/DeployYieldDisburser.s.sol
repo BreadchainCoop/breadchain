bcb7a7ada03b521fe87670028b63c472551040f1
Ron Turetzky <ron@eoracle.io>
2024-06-12 14:23:58 +0300

    fix: amending deploy script

--- script/deploy/DeployYieldDisburser.s.sol
+++ script/deploy/DeployYieldDisburser.s.sol
@@ -16,12 +16,12 @@ contract DeployYieldDisburser is Script {
     uint256 _blocktime = stdJson.readUint(config_data, "._blocktime");
     uint256 _minVotingAmount = stdJson.readUint(config_data, "._minVotingAmount");
     uint256 _minHoldingDuration = stdJson.readUint(config_data, "._minHoldingDuration");
-    uint256 _maxVotes = stdJson.readUint(config_data, "._maxVotes");
+    uint256 _cycleLength = stdJson.readUint(config_data, "._cycleLength");
     uint256 _maxPoints = stdJson.readUint(config_data, "._maxPoints");
     uint256 _minTimeBetweenClaims = stdJson.readUint(config_data, "._minTimeBetweenClaims");
     uint256 _precision = stdJson.readUint(config_data, "._precision");
     uint256 _lastClaimedTimestamp = stdJson.readUint(config_data, "._lastClaimedTimestamp");
-    uint256 _lastClaimedBlocknumber = stdJson.readUint(config_data, "._lastClaimedBlocknumber");
+    uint256 _lastClaimedBlockNumber = stdJson.readUint(config_data, "._lastClaimedBlockNumber");
     bytes projectsRaw = stdJson.parseRaw(config_data, "._projects");
     address[] projects = abi.decode(projectsRaw, (address[]));
     bytes initData = abi.encodeWithSelector(
@@ -31,9 +31,8 @@ contract DeployYieldDisburser is Script {
         _blocktime,
         _minVotingAmount,
         _minHoldingDuration,
-        _maxVotes,
         _maxPoints,
-        _minTimeBetweenClaims,
+        _cycleLength,
         _lastClaimedTimestamp,
         _lastClaimedBlocknumber,
         _precision
