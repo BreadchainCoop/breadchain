ec8e555f4a4f79dc3f301c8e9b95082176fa7a6e
Ron Turetzky <ron@eoracle.io>
2024-07-25 15:23:00 +0300

    chore: making upgrade script accept the address of the proxy to upgrade

--- script/upgrades/UpgradeYieldDistributor.s.sol
+++ script/upgrades/UpgradeYieldDistributor.s.sol
@@ -5,9 +5,8 @@ import {Options} from "openzeppelin-foundry-upgrades/Options.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployYieldDistributor is Script {
-    function run() external {
+    function run(address proxyAddress) external {
         vm.startBroadcast();
-        address proxyAddress = 0xeE95A62b749d8a2520E0128D9b3aCa241269024b;
         bytes memory data;
         Options memory opts;
         opts.referenceContract = "v1.0.0/YieldDistributor.sol:YieldDistributor";
