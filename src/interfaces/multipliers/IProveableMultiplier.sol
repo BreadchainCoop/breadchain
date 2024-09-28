e0d252779b18563c57a27299ee218cc91fa5c262
Ron Turetzky <turetzkyron@gmail.com>
2024-09-28 08:44:52 +0300

    chore: init derived interfaces

---
+++ src/interfaces/multipliers/IProveableMultiplier.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IDynamicNFTMultiplier} from "src/interfaces/multipliers/IDynamicNFTMultiplier.sol";
+/// @title Proveable Multiplier Interface
+/// @notice Interface for contracts that provide a proveable multiplying factor based on user activities
+
+interface IProveableMultiplier is IERC721, IDynamicNFTMultiplier {
+    /// @notice Submit activities to potentially earn or upgrade an NFT
+    /// @param data Encoded data representing the activities
+    function submitActivities(bytes calldata data) external;
+
+    /// @notice Mint a new NFT to a user (internal function in implementation)
+    /// @param to The address to mint the NFT to
+    /// @param tokenId The token ID of the NFT to mint
+    function mint(address to, uint256 tokenId) external;
+}
