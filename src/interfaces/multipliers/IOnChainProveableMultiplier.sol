47cd4e8f32525bd8db5d35f08093de602081ea48
Ron Turetzky <turetzkyron@gmail.com>
2024-12-24 19:05:42 -0500

    fmt: removing line break

--- src/interfaces/multipliers/IOnChainProveableMultiplier.sol
+++ src/interfaces/multipliers/IOnChainProveableMultiplier.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.22;
 import {IProveableMultiplier} from "src/interfaces/multipliers/IProveableMultiplier.sol";
 /// @title On-Chain Proveable Multiplier Interface
 /// @notice Interface for contracts that provide an on-chain proveable multiplying factor
-
 interface IOnChainProveableMultiplier is IProveableMultiplier {
     /// @notice Get the address of the activity contract
     /// @return The address of the contract used for verifying on-chain activities
