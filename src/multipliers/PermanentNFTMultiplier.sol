3e6738206a4f81485a60972b2f866f10968894c0
Ron Turetzky <turetzkyron@gmail.com>
2025-01-04 16:46:24 -0500

    Revert "perf: @daopunk https://github.com/BreadchainCoop/breadchain/pull/100\#discussion_r1901198370""
    This reverts commit fde3d9a2d6494dcaf1e1734d496a86760774db4d.

--- src/multipliers/PermanentNFTMultiplier.sol
+++ src/multipliers/PermanentNFTMultiplier.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {INFTMultiplier} from "src/interfaces/multipliers/INFTMultiplier.sol";
 
 /// @title Permanent NFT Multiplier
