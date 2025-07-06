3a782ed1148b400caba683afb0a82268f11e9c92
Sara <sara@Saras-MacBook-Pro.local>
2025-07-06 14:59:56 +0100

    Add DeployVotingStreakMultiplier
    [#135]

---
+++ script/deploy/DeployVotingStreakMultiplier.s.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
+import "forge-std/console.sol";
+import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import {VotingStreakMultiplier} from "../../src/multipliers/VotingStreakMultiplier.sol";
+
+contract DeployVotingStreakMultiplier is Script {
+    string public deployConfigPath = string(bytes("./script/deploy/config/deployVotingStreakMultiplier.json"));
+    string config_data = vm.readFile(deployConfigPath);
+    address _yieldDistributor = stdJson.readAddress(config_data, "._yieldDistributor");
+    uint256 _multiplierIncrement = stdJson.readUint(config_data, "._multiplierIncrement");
+    uint256 _maxMultiplierIncrements = stdJson.readUint(config_data, "._maxMultiplierIncrements");
+    address _owner = stdJson.readAddress(config_data, "._owner");
+
+    bytes initData = abi.encodeWithSelector(
+        VotingStreakMultiplier.initialize.selector, _yieldDistributor, _multiplierIncrement, _maxMultiplierIncrements
+    );
+
+    function run() external {
+        vm.startBroadcast();
+        VotingStreakMultiplier votingStreakMultiplierImplementation = new VotingStreakMultiplier();
+        VotingStreakMultiplier votingStreakMultiplier = VotingStreakMultiplier(
+            address(new TransparentUpgradeableProxy(address(votingStreakMultiplierImplementation), _owner, initData))
+        );
+        console2.log("Deployed VotingStreakMultiplier at address: {}", address(votingStreakMultiplier));
+        vm.stopBroadcast();
+    }
+}
